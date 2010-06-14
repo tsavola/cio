62d894d775c2b540123a83b883786b443115c17b
Timo Savola <timo.savola@iki.fi>
2010-06-14 23:14:48 +0300

    print errno if assert fails in unit test

--- test/assert.h
+++ test/assert.h
@@ -1,5 +1,19 @@
-#ifdef NDEBUG
-#undef NDEBUG
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#ifdef assert
+# undef assert
 #endif
 
-#include <assert.h>
+#define assert(expr) \
+	do { \
+		if (!(expr)) { \
+			if (errno) \
+				fprintf(stderr, "%s:%d: %s: assert(%s) failed: %s\n", __FILE__, __LINE__, __func__, #expr, strerror(errno)); \
+			else \
+				fprintf(stderr, "%s:%d: %s: assert(%s) failed\n", __FILE__, __LINE__, __func__, #expr); \
+			abort(); \
+		} \
+	} while (0)
